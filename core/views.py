496cb871a35154bb237ccb38fb61be035e23c06e
MidnightRoam <saldatsenka@list.ru>
2022-07-25 10:57:17 +0300

    Feedback api added

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
+from django.core.mail import send_mail
 from rest_framework import viewsets, permissions
-from .serializers import PostSerializer, TagSerializer
+from rest_framework.views import APIView
+
+from .serializers import PostSerializer, TagSerializer, ContactSerializer
 from .models import Post
 from rest_framework.response import Response
 from rest_framework import pagination
@@ -42,3 +45,19 @@ class AsideView(generics.ListAPIView):
     queryset = Post.objects.all().order_by('-id')[:5]
     serializer_class = PostSerializer
     permission_classes = [permissions.AllowAny]
+
+
+class FeedBackView(APIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = ContactSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer_class = ContactSerializer(data=request.data)
+        if serializer_class.is_valid():
+            data = serializer_class.validated_data
+            name = data.get('name')
+            from_email = data.get('email')
+            subject = data.get('subject')
+            message = data.get('message')
+            send_mail(f'From {name} | {subject}', message, from_email, ['midnightroam1@gmail.com'])
+            return Response({"success": "Sent"})
