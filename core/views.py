235bbe5e03e3cca429887e1a5aa8e1bd255e57b3
MidnightRoam <saldatsenka@list.ru>
2022-07-25 10:38:22 +0300

    first release v0.1

---
+++ core/views.py
@@ -0,0 +1,38 @@
+from rest_framework import viewsets, permissions
+from .serializers import PostSerializer, TagSerializer
+from .models import Post
+from rest_framework.response import Response
+from rest_framework import pagination
+from rest_framework import generics
+from taggit.models import Tag
+
+
+class PageNumberSetPagination(pagination.PageNumberPagination):
+    page_size = 3
+    page_size_query_param = 'page_size'
+    ordering = 'created_at'
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer
+    queryset = Post.objects.all()
+    lookup_field = 'slug'
+    permission_classes = [permissions.AllowAny]
+    permission_class = PageNumberSetPagination
+
+
+class TagDetailView(generics.ListAPIView):
+    serializer_class = PostSerializer
+    pagination_class = PageNumberSetPagination
+    permission_classes = [permissions.AllowAny]
+
+    def get_queryset(self):
+        tag_slug = self.kwargs['tag_slug'].lower()
+        tag = Tag.objects.get(slug=tag_slug)
+        return Post.objects.filter(tags=tag)
+
+
+class TagView(generics.ListAPIView):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [permissions.AllowAny]
